9768e49035e83d5f97bef475032d490c2e9b72ba
4qWyn <81542227+Wynazanar@users.noreply.github.com>
2024-11-27 15:25:39 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World!",
-  })
+  });
 })
 
 app.listen(5050, () => console.log("Listening on http:localhost:5050"));
